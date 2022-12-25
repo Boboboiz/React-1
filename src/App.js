6c00623126e608377d9dc62fa4f29dfbe0c89c4d
Huy <lenambaohuy107@gmail.com>
2022-12-25 11:26:33 +0700

    done hw1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+// import Header from './demoComponent/Header';
+// import Footer from './demoComponent/Footer';
+// import Home from './ex1/Home';
+import Landingpage from './React-hw1/Landingpage'
+function App() {
+  return (
+    <div className="App">
+      {/**Gọi component Header hiển thị */}
+      <Landingpage />
+    </div>
+  );
+}
+
+export default App;
